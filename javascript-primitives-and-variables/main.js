eb174f41db5bd777681e403eedc70764c44341b9
Jonathan Chiang <jc91765@gmail.com>
2021-07-12 18:51:40 -0700

    Completed javascript-primitives-and-variables - Jonathan Chiang

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,21 @@
+var fullName = 'Jonathan Chiang';
+var isCool = true;
+var totalpets = 0;
+
+console.log('value of fullName: ', fullName);
+console.log('value of isCool: ', isCool);
+console.log('value of totalPets: ', totalpets);
+
+console.log('typeof fullName: ', typeof fullName);
+console.log('typeof isCool: ', typeof isCool);
+console.log('typeof totalPet: ', typeof totalpets);
+
+var unkown = null;
+
+console.log('value of unknown: ', unkown);
+console.log('typeof unknown: ', typeof unkown);
+
+var never;
+
+console.log('value of never: ', never);
+console.log('typeof never: ', typeof never);
